cf683e7886fbcf06ee2d5d79a4952b9653f6b841
broxy-a <abdrahmanovamir2001@gmail.com>
2025-04-20 17:47:25 +0500

    Начало моддинга

---
+++ src/main/java/net/animals/capybaramod/CapybaraModClient.java
@@ -0,0 +1,10 @@
+package net.animals.capybaramod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CapybaraModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
